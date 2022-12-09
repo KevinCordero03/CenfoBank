3e9bfb74d3b79990df625c020f6616ed2989f6fa
Kevin Cordero <95945395+KevinCordero03@users.noreply.github.com>
2022-12-09 09:26:21 -0600

    Add files via upload

---
+++ WebUI/Scripts/Views/RegisterUser.js
@@ -0,0 +1,28 @@
+﻿//Controlador de la Vista: RegistrarUsuario.cshtml
+
+function CustomerView() {
+    //Atributos
+    this.ViewName = "CustomerView";
+    this.ApiUrl = "";
+
+    //JQueary framework que facilita el manejo de JS
+    //Se invoca con el simbolo de $
+    this.InitView = function () {
+        $("#btnCreate").click(function () {
+            var view = new CustomerView();
+            view.CreateCustomer();
+        });
+    }
+
+    this.CreateCustomer = function () {
+        alert("Creando Usuario...Espere!");
+
+    };
+}//
+
+//Instanciamiento inicial, siempre se ejecuta cuando la pagina termina de cargar.
+
+$(document).ready(function () {
+    var view = new CustomerView();
+    view.InitView();
+})
